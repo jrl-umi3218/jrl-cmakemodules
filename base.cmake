066279718f33ff54a99ca39ef20251ea5affedcb
Thomas Moulard <thomas.moulard@gmail.com>
2010-09-01 01:07:47 +0200

    Modify base.cmake and pkg-config.pc.cmake to fix various errors.
    Previous version was not working due to scope errors in CMake
    and string expansion problems.

--- base.cmake
+++ base.cmake
@@ -78,21 +78,39 @@ ENDIF(WIN32)
  # Project configuration #
  # --------------------- #
 
-# _ADD_TO_COMMA_LIST LIST VALUE
+# _ADD_TO_LIST LIST VALUE
 # -------------
 #
 # Add a value to a comma-separated list.
 #
-# LIST	: the comma-separated list.
-# VALUE : the value to be appended.
+# LIST		: the list.
+# VALUE		: the value to be appended.
+# SEPARATOR	: the separation symol.
 #
-FUNCTION(_ADD_TO_COMMA_LIST LIST VALUE)
-  IF(${LIST} STREQUAL "")
-    SET(LIST ${VALUE})
-  ELSE(${LIST} STREQUAL "")
-    SET(LIST "${LIST}, ${VALUE}")
-  ENDIF(${LIST} STREQUAL "")
-ENDFUNCTION(_ADD_TO_COMMA_LIST LIST VALUE)
+MACRO(_ADD_TO_LIST LIST VALUE SEPARATOR)
+  IF("${${LIST}}" STREQUAL "")
+    SET(${LIST} "${VALUE}")
+  ELSE("${${LIST}}" STREQUAL "")
+    SET(${LIST} "${${LIST}}${SEPARATOR} ${VALUE}")
+  ENDIF("${${LIST}}" STREQUAL "")
+ENDMACRO(_ADD_TO_LIST LIST VALUE)
+
+# _CONCATE_ARGUMENTS
+# -------------
+#
+# Concatenate all arguments into the output variable.
+#
+# OUTPUT	: the output variable.
+# SEPARTOR	: the list separation symbol.
+# ARG1...ARGN	: the values to be concatenated.
+#
+MACRO(_CONCATENATE_ARGUMENTS OUTPUT SEPARATOR)
+  FOREACH(I RANGE 2 ${ARGC})
+    _ADD_TO_LIST("${OUTPUT}" "${ARGV${I}}" "${SEPARATOR}")
+  ENDFOREACH(I RANGE 2 ${ARGC})
+  MESSAGE(${${OUTPUT}})
+ENDMACRO(_CONCATENATE_ARGUMENTS OUTPUT)
+
 
 
 # _SETUP_PROJECT_CPACK
@@ -100,7 +118,7 @@ ENDFUNCTION(_ADD_TO_COMMA_LIST LIST VALUE)
 #
 # Configure CPack as much as possible.
 #
-FUNCTION(_SETUP_PROJECT_CPACK)
+MACRO(_SETUP_PROJECT_CPACK)
   # CPack related variables definition.
 
   ## For tarball generation
@@ -117,7 +135,7 @@ FUNCTION(_SETUP_PROJECT_CPACK)
 
   ## For Debian
   # FIXME: to be done.
-ENDFUNCTION(_SETUP_PROJECT_CPACK)
+ENDMACRO(_SETUP_PROJECT_CPACK)
 
 
 # _SETUP_PROJECT_PKG_CONFIG
@@ -125,45 +143,37 @@ ENDFUNCTION(_SETUP_PROJECT_CPACK)
 #
 # Prepare pkg-config pc file generation step.
 #
-FUNCTION(_SETUP_PROJECT_PKG_CONFIG)
+MACRO(_SETUP_PROJECT_PKG_CONFIG)
   # Pkg-config related commands.
-  SET(PKG_CONFIG_PREFIX ${CMAKE_INSTALL_PREFIX})
-  SET(PKG_CONFIG_EXEC_PREFIX ${PKG_CONFIG_PREFIX})
-  SET(PKG_CONFIG_LIBDIR ${PKG_CONFIG_EXEC_PREFIX}/lib)
-  SET(PKG_CONFIG_INCLUDEDIR ${PKG_CONFIG_PREFIX}/include)
-  SET(PKG_CONFIG_DATAROOTDIR ${PKG_CONFIG_PREFIX}/share)
-  SET(PKG_CONFIG_DOCDIR ${PKG_CONFIG_DATAROOTDIR}/doc/${PROJECT_NAME}
-   )
-  SET(PKG_CONFIG_DOXYGENDOCDIR ${PKG_CONFIG_DOCDIR}/doxygen-html)
+  SET(PKG_CONFIG_PREFIX "${CMAKE_INSTALL_PREFIX}")
+  SET(PKG_CONFIG_EXEC_PREFIX "${PKG_CONFIG_PREFIX}")
+  SET(PKG_CONFIG_LIBDIR "${PKG_CONFIG_EXEC_PREFIX}/lib")
+  SET(PKG_CONFIG_INCLUDEDIR "${PKG_CONFIG_PREFIX}/include")
+  SET(PKG_CONFIG_DATAROOTDIR "${PKG_CONFIG_PREFIX}/share")
+  SET(PKG_CONFIG_DOCDIR "${PKG_CONFIG_DATAROOTDIR}/doc/${PROJECT_NAME}")
+  SET(PKG_CONFIG_DOXYGENDOCDIR "${PKG_CONFIG_DOCDIR}/doxygen-html")
 
-  SET(PKG_CONFIG_PROJECT_NAME ${PROJECT_NAME})
-  SET(PKG_CONFIG_DESCRIPTION)
-  SET(PKG_CONFIG_URL)
-  SET(PKG_CONFIG_VERSION ${PROJECT_VERSION})
-  SET(PKG_CONFIG_REQUIRES)
-  SET(PKG_CONFIG_CONFLICTS)
-  SET(PKG_CONFIG_LIBS)
-  SET(PKG_CONFIG_LIBS_PRIVATE)
-  SET(PKG_CONFIG_LIBS_CFLAGS)
+  SET(PKG_CONFIG_PROJECT_NAME "${PROJECT_NAME}")
+  SET(PKG_CONFIG_DESCRIPTION "")
+  SET(PKG_CONFIG_URL "")
+  SET(PKG_CONFIG_VERSION "${PROJECT_VERSION}")
+  SET(PKG_CONFIG_REQUIRES "")
+  SET(PKG_CONFIG_CONFLICTS "")
+  SET(PKG_CONFIG_LIBS "")
+  SET(PKG_CONFIG_LIBS_PRIVATE "")
+  SET(PKG_CONFIG_LIBS_CFLAGS "")
 
-  SET(PKG_CONFIG_EXTRA)
+  SET(PKG_CONFIG_EXTRA "")
 
   # Where to install the pkg-config file?
-  SET(PKG_CONFIG_DIR ${PKG_CONFIG_LIBDIR}/pkgconfig)
-  MESSAGE(STATUS ${PKG_CONFIG_DIR})
-
-  # Generate the file.
-  CONFIGURE_FILE(
-    ${CMAKE_CURRENT_SOURCE_DIR}/cmake/pkg-config.pc.cmake
-    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc
-    )
+  SET(PKG_CONFIG_DIR "${PKG_CONFIG_LIBDIR}/pkgconfig")
 
   # Install it.
   INSTALL(
-    FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc
-    DESTINATION ${PKG_CONFIG_DIR}
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc"
+    DESTINATION "${PKG_CONFIG_DIR}"
     PERMISSIONS OWNER_READ GROUP_READ WORLD_READ OWNER_WRITE)
-ENDFUNCTION(_SETUP_PROJECT_PKG_CONFIG)
+ENDMACRO(_SETUP_PROJECT_PKG_CONFIG)
 
 
 # _SETUP_PROJECT_UNINSTALL
@@ -171,12 +181,12 @@ ENDFUNCTION(_SETUP_PROJECT_PKG_CONFIG)
 #
 # Add custom rule to uninstall the package.
 #
-FUNCTION(_SETUP_PROJECT_UNINSTALL)
+MACRO(_SETUP_PROJECT_UNINSTALL)
   # FIXME: it is utterly stupid to rely on the install manifest.
   # Can't we just remember what we install ?!
   CONFIGURE_FILE(
-    ${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake"
     IMMEDIATE
     )
 
@@ -185,7 +195,7 @@ FUNCTION(_SETUP_PROJECT_UNINSTALL)
     "${CMAKE_COMMAND}" -P
     "${CMAKE_CURRENT_BINARY_DIR}/cmake/cmake_uninstall.cmake"
     )
-ENDFUNCTION(_SETUP_PROJECT_UNINSTALL)
+ENDMACRO(_SETUP_PROJECT_UNINSTALL)
 
 
 # SETUP_PROJECT
@@ -198,7 +208,7 @@ ENDFUNCTION(_SETUP_PROJECT_UNINSTALL)
 # variables are defined (see documentation at the beginning of this
 # file).
 #
-FUNCTION(SETUP_PROJECT)
+MACRO(SETUP_PROJECT)
   # Check that required variables are defined.
   FOREACH(VARIABLE ${REQUIRED_VARIABLES})
     IF (NOT DEFINED ${VARIABLE})
@@ -211,13 +221,27 @@ FUNCTION(SETUP_PROJECT)
   PROJECT(${PROJECT_NAME} CXX)
 
   # Be verbose by default.
-  SET(CMAKE_VERBOSE_MAKEFILE true)
+  SET(CMAKE_VERBOSE_MAKEFILE TRUE)
 
 
   _SETUP_PROJECT_CPACK()
   _SETUP_PROJECT_UNINSTALL()
   _SETUP_PROJECT_PKG_CONFIG()
-ENDFUNCTION(SETUP_PROJECT)
+ENDMACRO(SETUP_PROJECT)
+
+# SETUP_PROJECT_FINALIZE
+# ----------------------
+#
+# To be called at the end of the CMakeLists.txt to
+# finazlie the project setup.
+#
+MACRO(SETUP_PROJECT_FINALIZE)
+  # Generate the pkg-config file.
+  CONFIGURE_FILE(
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/pkg-config.pc.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc"
+    )
+ENDMACRO(SETUP_PROJECT_FINALIZE)
 
 
 # ADD_REQUIRED_DEPENDENCY(PREFIX PKGCONFIG_STRING)
@@ -226,32 +250,48 @@ ENDFUNCTION(SETUP_PROJECT)
 # Check for a dependency using pkg-config. Fail if the package cannot
 # be found.
 #
-# PREFIX		: prefix used to generate CMake variables.
-# LIBRARY_NAME		: name of the pkg-config library to check.
-#			  Typically, this string looks like ``my-package''.
 # PKG_CONFIG_STRING	: string passed to pkg-config to check the version.
-#			  Typically, this string looks like: ``my-package >= 0.5''
+#			  Typically, this string looks like:
+#                         ``my-package >= 0.5''
 #
-FUNCTION(ADD_REQUIRED_DEPENDENCY PREFIX LIBRARY_NAME PKG_CONFIG_STRING)
+MACRO(ADD_REQUIRED_DEPENDENCY PKG_CONFIG_STRING)
+  # Retrieve the left part of the equation to get package name.
+  STRING(REGEX MATCH "[^<>= ]+" LIBRARY_NAME "${PKG_CONFIG_STRING}")
+  # And transform it into a valid variable prefix.
+  # 1. replace invalid characters into underscores.
+  STRING(REGEX REPLACE "[^a-zA-Z0-9]" "_" PREFIX "${LIBRARY_NAME}")
+  # 2. make it uppercase.
+  STRING(TOUPPER "${PREFIX}" "PREFIX")
+
   # Search for the package.
-  PKG_CHECK_MODULES(${PREFIX} REQUIRED ${PKG_CONFIG_STRING})
+  PKG_CHECK_MODULES("${PREFIX}" REQUIRED "${PKG_CONFIG_STRING}")
 
   # Get the values of additional variables.
-  FOREACH(VARIABLE ${REQUIRED_VARIABLES})
+  FOREACH(VARIABLE ${PKG_CONFIG_ADDITIONAL_VARIABLES})
     # Upper-case version of the variable for CMake variable generation.
-    STRING(TOUPPER ${VARIABLE} ${VARIABLE}_UC)
+    STRING(TOUPPER "${VARIABLE}" "${VARIABLE}_UC")
     EXEC_PROGRAM(
-      ${PKG_CONFIG_EXECUTABLE} ARGS  --variable=${VARIABLE} ${LIBRARY_NAME}
-      OUTPUT_VARIABLE ${PREFIX}_${VARIABLE_UC})
+      "${PKG_CONFIG_EXECUTABLE}" ARGS
+      "--variable=${VARIABLE}" "${LIBRARY_NAME}"
+      OUTPUT_VARIABLE "${PREFIX}_${VARIABLE_UC}")
   ENDFOREACH(VARIABLE)
 
+  #FIXME: spaces are replaced by semi-colon by mistakes, revert the change.
+  #I cannot see why CMake is doing that...
+  STRING(REPLACE ";" " " PKG_CONFIG_STRING "${PKG_CONFIG_STRING}")
+  STRING(REPLACE ";" " " "${PREFIX}_CFLAGS" "${${PREFIX}_CFLAGS}")
+  STRING(REPLACE ";" " " "${PREFIX}_LDFLAGS" "${${PREFIX}_LDFLAGS}")
 
   # Add the package to the dependency list.
-  _ADD_TO_COMMA_LIST(${PKG_CONFIG_REQUIRES}, ${PKG_CONFIG_STRING})
+  _ADD_TO_LIST(PKG_CONFIG_REQUIRES "${PKG_CONFIG_STRING}" ",")
 
   # Add to the library list.
-  _ADD_TO_COMMA_LIST(${PKG_CONFIG_LIBS}, ${${PREFIX}_LDFLAGS})
+  _ADD_TO_LIST(PKG_CONFIG_LIBS "${${PREFIX}_LDFLAGS}" "")
 
   # Add to the include list.
-  _ADD_TO_COMMA_LIST(${PKG_CONFIG_INCLUDE}, ${${PREFIX}_CFLAGS})
-ENDFUNCTION(ADD_REQUIRED_DEPENDENCY)
+  _ADD_TO_LIST(PKG_CONFIG_CFLAGS "${${PREFIX}_CFLAGS}" "")
+
+  MESSAGE(STATUS
+    "Pkg-config module ${LIBRARY_NAME} v${${PREFIX}_VERSION}"
+    " has been detected with success.")
+ENDMACRO(ADD_REQUIRED_DEPENDENCY)
