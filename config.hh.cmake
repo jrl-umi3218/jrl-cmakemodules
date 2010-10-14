5779fce0ea1add634dc59f5259ac265b849525b9
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-14 13:02:25 +0200

    Fix template config.hh.

--- config.hh.cmake
+++ config.hh.cmake
@@ -8,25 +8,54 @@
 //
 // See the COPYING file for more information.
 
-#ifndef ${PACKAGE_CPPNAME}_CONFIG_HH
-# define ${PACKAGE_CPPNAME}_CONFIG_HH
+#ifndef @PACKAGE_CPPNAME@_CONFIG_HH
+# define @PACKAGE_CPPNAME@_CONFIG_HH
 
 // Package version (header).
-# define ${PACKAGE_CPPNAME}_VERSION "${PROJECT_VERSION}"
+# define @PACKAGE_CPPNAME@_VERSION "@PROJECT_VERSION@"
 
-# ifdef ${PACKAGE_CPPNAME}_STATIC
+// Handle portable symbol export.
+// Defining manually which symbol should be exported is required
+// under Windows whether MinGW or MSVC is used.
+//
+// The headers then have to be able to work in two different modes:
+// - dllexport when one is building the library,
+// - dllimport for clients using the library.
+//
+// On Linux, set the visibility accordingly. If C++ symbol visibility
+// is handled by the compiler, see: http://gcc.gnu.org/wiki/Visibility
+# if defined _WIN32 || defined __CYGWIN__
+// On Microsoft Windows, use dllimport and dllexport to tag symbols.
+#  define @PACKAGE_CPPNAME@_DLLIMPORT __declspec(dllimport)
+#  define @PACKAGE_CPPNAME@_DLLEXPORT __declspec(dllexport)
+#  define @PACKAGE_CPPNAME@_DLLLOCAL
+# else
+// On Linux, for GCC >= 4, tag symbols using GCC extension.
+#  if __GNUC__ >= 4
+#   define @PACKAGE_CPPNAME@_DLLIMPORT __attribute__ ((visibility("default")))
+#   define @PACKAGE_CPPNAME@_DLLEXPORT __attribute__ ((visibility("default")))
+#   define @PACKAGE_CPPNAME@_DLLLOCAL  __attribute__ ((visibility("hidden")))
+#  else
+// Otherwise (GCC < 4 or another compiler is used), export everything.
+#   define @PACKAGE_CPPNAME@_DLLIMPORT
+#   define @PACKAGE_CPPNAME@_DLLEXPORT
+#   define @PACKAGE_CPPNAME@_DLLLOCAL
+#  endif // __GNUC__ >= 4
+# endif // defined _WIN32 || defined __CYGWIN__
+
+# ifdef @PACKAGE_CPPNAME@_STATIC
 // If one is using the library statically, get rid of
 // extra information.
-#  define ${PACKAGE_CPPNAME}_DLLAPI
-#  define ${PACKAGE_CPPNAME}_LOCAL
+#  define @PACKAGE_CPPNAME@_DLLAPI
+#  define @PACKAGE_CPPNAME@_LOCAL
 # else
 // Depending on whether one is building or using the
 // library define DLLAPI to import or export.
-#  ifdef BUILDING_${PACKAGE_CPPNAME}
-#   define ${PACKAGE_CPPNAME}_DLLAPI ${PACKAGE_CPPNAME}_DLLEXPORT
+#  ifdef BUILDING_@PACKAGE_CPPNAME@
+#   define @PACKAGE_CPPNAME@_DLLAPI @PACKAGE_CPPNAME@_DLLEXPORT
 #  else
-#   define ${PACKAGE_CPPNAME}_DLLAPI ${PACKAGE_CPPNAME}_DLLIMPORT
-#  endif // BUILDING_${PACKAGE_CPPNAME}
-#  define ${PACKAGE_CPPNAME}_LOCAL ${PACKAGE_CPPNAME}_DLLLOCAL
-# endif // ${PACKAGE_CPPNAME}_STATIC
-#endif //! ${PACKAGE_CPPNAME}_CONFIG_HH
+#   define @PACKAGE_CPPNAME@_DLLAPI @PACKAGE_CPPNAME@_DLLIMPORT
+#  endif // BUILDING_@PACKAGE_CPPNAME@
+#  define @PACKAGE_CPPNAME@_LOCAL @PACKAGE_CPPNAME@_DLLLOCAL
+# endif // @PACKAGE_CPPNAME@_STATIC
+#endif //! @PACKAGE_CPPNAME@_CONFIG_HH
