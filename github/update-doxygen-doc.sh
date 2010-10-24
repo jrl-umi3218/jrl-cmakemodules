d2efc0844a20b4c6f4d3fb6c61b2ff604e0fa91f
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-24 14:35:38 +0200

    Fix bug while copying documentation in github/upate-doxygen-doc.sh.

--- github/update-doxygen-doc.sh
+++ github/update-doxygen-doc.sh
@@ -225,7 +225,7 @@ ${GIT} checkout --quiet -b gh-pages origin/gh-pages \
 
 echo "* Copy the documentation..."
 mkdir -p doxygen
-cp -r $build_docdir/doxygen-html doxygen/$doc_version \
+cp -r $build_docdir/doxygen-html/ doxygen/$doc_version \
  || abort "failed to copy the documentation"
 
 echo "* Generate the commit..."
