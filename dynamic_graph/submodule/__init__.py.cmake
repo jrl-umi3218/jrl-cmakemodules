734376a94e3283fa927be619f75e5586b7f6647a
florent <florent@laas.fr>
2010-12-16 17:39:40 +0100

    Import C++ symbols in global scope to make signals work together.
           * dynamic_graph/submodule/__init__.py.cmake.

--- dynamic_graph/submodule/__init__.py.cmake
+++ dynamic_graph/submodule/__init__.py.cmake
@@ -3,8 +3,15 @@
 
     Author: Florent Lamiraux
 """
+import sys, dl
 import dynamic_graph as dg
+flags = sys.getdlopenflags()
+# Import C++ symbols in a global scope
+# This is necessary for signal compiled in different modules to be compatible
+sys.setdlopenflags(dl.RTLD_NOW|dl.RTLD_GLOBAL)
 import wrap
+# Recover previous flags
+sys.setdlopenflags(flags)
 
 dg.entity.updateEntityClasses(globals())
 
