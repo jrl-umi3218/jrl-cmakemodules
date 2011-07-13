2bdf972bdc8d27bc9a3fdd7251d04d1d5a7ab359
olivier-stasse <olivier.stasse@aist.go.jp>
2011-07-13 16:50:02 +0900

    Generate file for ld.so.conf. Install is done only when building and installing the debian package.

--- debian.cmake
+++ debian.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2010 Florent Lamiraux, Thomas Moulard, JRL, CNRS/AIST.
+# Copyright (C) 2011 Olivier Stasse, JRL, CNRS/AIST.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,12 +13,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# _SETUP_DEBIAN
+# -------------------
+#
+# Prepare needed file for debian if the target is a shared library.
+#
+MACRO(_SETUP_DEBIAN)
+  MESSAGE(STATUS "setup debian. Trying to get the type of ${PROJECT_NAME}")
+  GET_TARGET_PROPERTY(${PROJECT_NAME}_IS_SHARED_LIBRARY ${PROJECT_NAME} TYPE)
+  MESSAGE(STATUS "${PROJECT_NAME}_IS_SHARED_LIBRARY:" ${${PROJECT_NAME}_IS_SHARED_LIBRARY})
+
+  IF(UNIX)
+    IF (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/debian)
+      IF(${PROJECT_NAME}_IS_SHARED_LIBRARY STREQUAL "SHARED_LIBRARY")
+	 # Create the install file to be inside ld.so.conf.d
+	 SET(install_file_name "debian/install")
+         FILE(WRITE ${install_file_name}
+	   "debian/lib${PROJECT_NAME}.conf /etc/ld.so.conf.d"
+	   )
+	 # Create the file to be installed.
+	 SET(install_file_name "debian/lib${PROJECT_NAME}.conf")
+	 FILE(WRITE ${install_file_name}
+	   "${CMAKE_INSTALL_PREFIX}/lib"
+	   )
+      ENDIF(${PROJECT_NAME}_IS_SHARED_LIBRARY STREQUAL "SHARED_LIBRARY")
+    ENDIF(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/debian)
+  ENDIF(UNIX)
+ENDMACRO(_SETUP_DEBIAN)
+
 # _SETUP_PROJECT_DEB
 # -------------------
 #
 # Add a deb target to generate a Debian package using
 # git-buildpackage (Linux specific).
 #
+
 MACRO(_SETUP_PROJECT_DEB)
   IF(UNIX)
   ADD_CUSTOM_TARGET(deb-src
@@ -37,3 +66,4 @@ MACRO(_SETUP_PROJECT_DEB)
     )
   ENDIF(UNIX)
 ENDMACRO(_SETUP_PROJECT_DEB)
+
