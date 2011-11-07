3de45376c982e2b4a0cfc3b096ff7eb280186965
Florent Lamiraux <florent@laas.fr>
2011-11-07 17:14:42 +0100

    Replace dl by DLFCN
        Since python2.6, dl module is deprecated.

--- dynamic_graph/submodule/__init__.py.cmake
+++ dynamic_graph/submodule/__init__.py.cmake
@@ -3,12 +3,12 @@
 
     Author: Florent Lamiraux
 """
-import sys, dl
+import sys, DLFCN
 import dynamic_graph as dg
 flags = sys.getdlopenflags()
 # Import C++ symbols in a global scope
 # This is necessary for signal compiled in different modules to be compatible
-sys.setdlopenflags(dl.RTLD_NOW|dl.RTLD_GLOBAL)
+sys.setdlopenflags(DLFCN.RTLD_NOW|DLFCN.RTLD_GLOBAL)
 import wrap
 # Recover previous flags
 sys.setdlopenflags(flags)
