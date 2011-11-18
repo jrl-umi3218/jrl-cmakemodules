745ccfa1e1631ac110274ffcc084ebc6a9827ec9
Olivier Stasse <olivier.stasse@gmail.com>
2011-11-18 17:06:49 +0100

    Remove the P_REQUIRED test.

--- pkg-config.cmake
+++ pkg-config.cmake
@@ -181,9 +181,7 @@ MACRO(ADD_DEPENDENCY P_REQUIRED PKG_CONFIG_STRING)
 
   # Add the package to the dependency list if found
   IF(${PREFIX}_FOUND)
-    IF (P_REQUIRED)
-      _ADD_TO_LIST(PKG_CONFIG_REQUIRES "${PKG_CONFIG_STRING}" ",")
-    ENDIF(P_REQUIRED)
+    _ADD_TO_LIST(PKG_CONFIG_REQUIRES "${PKG_CONFIG_STRING}" ",")
   ENDIF(${PREFIX}_FOUND)
 
   # Add the package to the cmake dependency list
