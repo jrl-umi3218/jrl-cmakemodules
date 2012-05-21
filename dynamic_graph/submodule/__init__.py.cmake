99cfe509566adbf5308ce712d55ec77ccabcf2ab
Florent Lamiraux <florent@laas.fr>
2012-05-21 16:33:21 +0200

    Specify license.

--- dynamic_graph/submodule/__init__.py.cmake
+++ dynamic_graph/submodule/__init__.py.cmake
@@ -1,8 +1,20 @@
-"""
-    Copyright 2010 CNRS
+#
+#    Copyright 2010 CNRS
+#
+#    Author: Florent Lamiraux
+#
+# This file is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Lesser Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with
+# this file. If not, see <http://www.gnu.org/licenses/>.
 
-    Author: Florent Lamiraux
-"""
 import sys, DLFCN
 import dynamic_graph as dg
 flags = sys.getdlopenflags()
