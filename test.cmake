b52dbcf50991decbe7abf829073daf962c268c8d
Joseph Mirabel <josephmirabel@gmail.com>
2017-09-29 10:11:22 +0200

    Add DISABLE_TESTS support and ADD_UNIT_TEST macro (#115)
    * Add option COMPILE_UNIT_TESTS to exclude unit tests from target all
    
    * Update unit-test compilation depending on DISABLE_TESTS option
    
    * Remove forgottent COMPILE_UNIT_TESTS.

--- test.cmake
+++ test.cmake
@@ -13,13 +13,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Configure the unit test compilation
+IF(NOT DEFINED DISABLE_TESTS)
+  SET(DISABLE_TESTS OFF)
+ENDIF(NOT DEFINED DISABLE_TESTS)
+IF(DISABLE_TESTS)
+  ADD_TEST(ctest_build_tests "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR} --target build_tests)
+  ADD_CUSTOM_TARGET(build_tests)
+ENDIF(DISABLE_TESTS)
+
+# ADD_UNIT_TEST(NAME)
+# ----------------------
+#
+# The behaviour of this function depends on DISABLE_TESTS option:
+# - if OFF (default), nothing special is done.
+# - if ON, the unit-test is not compiled with target all.
+#          A target build_tests is added to compile the tests and
+#          a test that run target build_tests is run before all other tests.
+#
+MACRO(ADD_UNIT_TEST NAME SOURCE)
+  IF(DISABLE_TESTS)
+    ADD_EXECUTABLE(${NAME} EXCLUDE_FROM_ALL ${SOURCE})
+    ADD_DEPENDENCIES(build_tests ${NAME})
+    ADD_TEST(${NAME} ${RUNTIME_OUTPUT_DIRECTORY}/${NAME})
+    SET_TESTS_PROPERTIES ( ${NAME} PROPERTIES DEPENDS ctest_build_tests)
+  ELSE(DISABLE_TESTS)
+    ADD_EXECUTABLE(${NAME} ${SOURCE})
+    ADD_TEST(${NAME} ${RUNTIME_OUTPUT_DIRECTORY}/${NAME})
+  ENDIF(DISABLE_TESTS)
+ENDMACRO(ADD_UNIT_TEST NAME SOURCE)
+
 # DEFINE_UNIT_TEST(NAME LIB)
 # ----------------------
 #
 # Compile a program and add it as a test
 #
 MACRO(DEFINE_UNIT_TEST NAME LIB)
-  ADD_EXECUTABLE(${NAME} ${NAME}.cc)
+  ADD_UNIT_TEST(${NAME} ${NAME}.cc)
   TARGET_LINK_LIBRARIES(${NAME} ${LIB})
-  ADD_TEST(${NAME} ${RUNTIME_OUTPUT_DIRECTORY}/${NAME})
 ENDMACRO(DEFINE_UNIT_TEST)
