92a1c8f4daaf22198dc3de06ccf30e571c4bf74b
Justin Carpentier <justin.carpentier@inria.fr>
2018-10-18 19:26:06 +0200

    correct copyright

--- version.cmake
+++ version.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2008-2014,2018 LAAS-CNRS, JRL AIST-CNRS, INRIA.
+# Copyright (C) 2008-2018 LAAS-CNRS, JRL AIST-CNRS, INRIA.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
