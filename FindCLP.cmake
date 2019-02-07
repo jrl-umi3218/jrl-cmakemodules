a306e5fb93f84de50955231b6a99905525534125
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-02-07 15:30:36 +0100

    add copyright notices

--- FindCLP.cmake
+++ FindCLP.cmake
@@ -1,3 +1,22 @@
+#
+#   Copyright 2019 CNRS
+#
+#   Author: Guilhem Saurel
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 # Try to find CLP
 # in standard prefixes and in ${CLP_PREFIX}
 # Once done this will define
