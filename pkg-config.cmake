jrl-umi3218/jrl-cmakemodules

commit f34901f143d843b48dfdb8d9e904503ed96e2310

[pkg-config] NO_INCLUDE_SYSTEM option for PKG_CONFIG_USE_DEPENDENCY

pkg-config.cmake
@@ -724,7 +724,7 @@ ENDMACRO(PKG_CONFIG_APPEND_LIBS)
 #
 # I.e. PKG_CONFIG_USE_LCOMPILE_DEPENDENCY(my-binary my-package)
 #
-MACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY TARGET PREFIX)
+MACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY TARGET PREFIX NO_INCLUDE_SYSTEM)
 
   # COMPILE_OPTION variable appeared only since CMake 2.8.12
   IF(${CMAKE_VERSION} VERSION_LESS 2.8.12)
@@ -761,9 +761,16 @@ MACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY TARGET PREFIX)
 
   # Include/libraries paths seems to be filtered on Linux, add paths
   # again.
-  INCLUDE_DIRECTORIES(SYSTEM ${${PREFIX}_INCLUDE_DIRS})
+  IF(NOT NO_INCLUDE_SYSTEM)
+    SET(INCLUDE_DIRECTORIES_VAR "SYSTEM ${${PREFIX}_INCLUDE_DIRS}")
+    SET(DEBUG_INCLUDE_DIRECTORIES_VAR "SYSTEM ${${PREFIX}_DEBUG_INCLUDE_DIRS}")
+  ELSE(NOT NO_INCLUDE_SYSTEM)
+    SET(INCLUDE_DIRECTORIES_VAR "${${PREFIX}_INCLUDE_DIRS}")
+    SET(DEBUG_INCLUDE_DIRECTORIES_VAR "${${PREFIX}_DEBUG_INCLUDE_DIRS}")    
+  ENDIF(NOT NO_INCLUDE_SYSTEM)
+  INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES_VAR} )
   IF(DEFINED ${PREFIX}_DEBUG_FOUND)
-    INCLUDE_DIRECTORIES(SYSTEM ${${PREFIX}_DEBUG_INCLUDE_DIRS})
+    INCLUDE_DIRECTORIES(${DEBUG_INCLUDE_DIRECTORIES_VAR})
   ENDIF()
 
 ENDMACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY)
@@ -892,8 +899,15 @@ ENDMACRO(BUILD_PREFIX_FOR_PKG)
 #       PKG_CONFIG_USE_DEPENDENCY(my-binary my-package)
 #
 MACRO(PKG_CONFIG_USE_DEPENDENCY TARGET DEPENDENCY)
+  SET(options OPTIONAL NO_INCLUDE_SYSTEM)
+  SET(oneValueArgs )
+  SET(multiValueArgs )
+  cmake_parse_arguments(PKG_CONFIG_USE_DEPENDENCY
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
   BUILD_PREFIX_FOR_PKG(${DEPENDENCY} PREFIX)
-  PKG_CONFIG_USE_LCOMPILE_DEPENDENCY(${TARGET} ${PREFIX})
+  PKG_CONFIG_USE_LCOMPILE_DEPENDENCY(${TARGET}
+    ${PREFIX}
+    ${PKG_CONFIG_USE_DEPENDENCY_NO_INCLUDE_SYSTEM})
   PKG_CONFIG_USE_LLINK_DEPENDENCY(${TARGET} ${PREFIX})
 ENDMACRO(PKG_CONFIG_USE_DEPENDENCY TARGET DEPENDENCY)
 
@@ -908,8 +922,15 @@ ENDMACRO(PKG_CONFIG_USE_DEPENDENCY TARGET DEPENDENCY)
 #     when using a dependency detected through pkg-config.
 #
 MACRO(PKG_CONFIG_USE_COMPILE_DEPENDENCY TARGET DEPENDENCY)
+  SET(options OPTIONAL NO_INCLUDE_SYSTEM)
+  SET(oneValueArgs )
+  SET(multiValueArgs )
+  cmake_parse_arguments(PKG_CONFIG_USE_DEPENDENCY
+    "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
   BUILD_PREFIX_FOR_PKG(${DEPENDENCY} PREFIX)
-  PKG_CONFIG_USE_LCOMPILE_DEPENDENCY(${TARGET} ${PREFIX})
+  PKG_CONFIG_USE_LCOMPILE_DEPENDENCY(${TARGET}
+    ${PREFIX}
+    ${PKG_CONFIG_USE_DEPENDENCY_NO_INCLUDE_SYSTEM})
 ENDMACRO(PKG_CONFIG_USE_COMPILE_DEPENDENCY TARGET DEPENDENCY)
 
 #.rst:

commit 201b19e99e15689b12454e16057f935d052180cc

Fixes the issues introduced in #199

pkg-config.cmake
@@ -762,15 +762,16 @@ MACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY TARGET PREFIX NO_INCLUDE_SYSTEM)
   # Include/libraries paths seems to be filtered on Linux, add paths
   # again.
   IF(NOT NO_INCLUDE_SYSTEM)
-    SET(INCLUDE_DIRECTORIES_VAR "SYSTEM ${${PREFIX}_INCLUDE_DIRS}")
-    SET(DEBUG_INCLUDE_DIRECTORIES_VAR "SYSTEM ${${PREFIX}_DEBUG_INCLUDE_DIRS}")
-  ELSE(NOT NO_INCLUDE_SYSTEM)
-    SET(INCLUDE_DIRECTORIES_VAR "${${PREFIX}_INCLUDE_DIRS}")
-    SET(DEBUG_INCLUDE_DIRECTORIES_VAR "${${PREFIX}_DEBUG_INCLUDE_DIRS}")    
-  ENDIF(NOT NO_INCLUDE_SYSTEM)
-  INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES_VAR} )
+    INCLUDE_DIRECTORIES(SYSTEM ${${PREFIX}_INCLUDE_DIRS})
+  ELSE()
+    INCLUDE_DIRECTORIES(${${PREFIX}_INCLUDE_DIRS})
+  ENDIF()
   IF(DEFINED ${PREFIX}_DEBUG_FOUND)
-    INCLUDE_DIRECTORIES(${DEBUG_INCLUDE_DIRECTORIES_VAR})
+    IF(NOT NO_INCLUDE_SYSTEM)
+      INCLUDE_DIRECTORIES(SYSTEM ${${PREFIX}_DEBUG_INCLUDE_DIRS})
+    ELSE()
+      INCLUDE_DIRECTORIES( ${${PREFIX}_DEBUG_INCLUDE_DIRS})
+    ENDIF()
   ENDIF()
 
 ENDMACRO(PKG_CONFIG_USE_LCOMPILE_DEPENDENCY)
@@ -899,7 +900,7 @@ ENDMACRO(BUILD_PREFIX_FOR_PKG)
 #       PKG_CONFIG_USE_DEPENDENCY(my-binary my-package)
 #
 MACRO(PKG_CONFIG_USE_DEPENDENCY TARGET DEPENDENCY)
-  SET(options OPTIONAL NO_INCLUDE_SYSTEM)
+  SET(options NO_INCLUDE_SYSTEM)
   SET(oneValueArgs )
   SET(multiValueArgs )
   cmake_parse_arguments(PKG_CONFIG_USE_DEPENDENCY
@@ -922,7 +923,7 @@ ENDMACRO(PKG_CONFIG_USE_DEPENDENCY TARGET DEPENDENCY)
 #     when using a dependency detected through pkg-config.
 #
 MACRO(PKG_CONFIG_USE_COMPILE_DEPENDENCY TARGET DEPENDENCY)
-  SET(options OPTIONAL NO_INCLUDE_SYSTEM)
+  SET(options NO_INCLUDE_SYSTEM)
   SET(oneValueArgs )
   SET(multiValueArgs )
   cmake_parse_arguments(PKG_CONFIG_USE_DEPENDENCY