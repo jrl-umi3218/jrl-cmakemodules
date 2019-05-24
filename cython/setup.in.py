f374fdfc66efddecac63d7e2ad3d913b12f00fb5
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-05-24 14:58:40 +0900

    [cython] Fix script generation with empty libraries

--- cython/setup.in.py
+++ cython/setup.in.py
@@ -50,8 +50,8 @@ class pkg_config(object):
         self.include_dirs.append('@CMAKE_CURRENT_SOURCE_DIR@/include')
         library_dirs = "@CYTHON_BINDINGS_LINK_FLAGS@"
         self.library_dirs = [ x for x in library_dirs.split(';') if len(x) ]
-        self.libraries = "@CYTHON_BINDINGS_LIBRARIES@".split(";")
-        self.library_dirs += [os.path.dirname(l) for l in "@CYTHON_BINDINGS_TARGET_FILES@".split(';')]
+        self.libraries = [ l for l in "@CYTHON_BINDINGS_LIBRARIES@".split(";") if len(l) ]
+        self.library_dirs += [os.path.dirname(l) for l in "@CYTHON_BINDINGS_TARGET_FILES@".split(';') if len(l) ]
 
 config = pkg_config()
 
