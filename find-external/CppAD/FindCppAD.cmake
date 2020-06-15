5f207c172051003dfc5ff4738a5afdcf89ed5e25
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-15 11:28:30 +0200

    externals: fix CppAD

--- find-external/CppAD/FindCppAD.cmake
+++ find-external/CppAD/FindCppAD.cmake
@@ -1,26 +1,26 @@
 #
-#   Copyright 2020 CNRS
+#   Copyright 2020 CNRS INRIA
 #
 #   Author: Guilhem Saurel
 #
 
-# Try to find CppAD
-# in standard prefixes and in ${CppAD_PREFIX}
+# Try to find cppad
+# in standard prefixes and in ${cppad_PREFIX}
 # Once done this will define
-#  CppAD_FOUND - System has CppAD
-#  CppAD_INCLUDE_DIRS - The CppAD include directories
-#  CppAD_LIBRARIES - The libraries needed to use CppAD
-#  CppAD_DEFINITIONS - Compiler switches required for using CppAD
+#  cppad_FOUND - System has cppad
+#  cppad_INCLUDE_DIR - The cppad include directories
+#  cppad_LIBRARY - The libraries needed to use cppad
+#  cppad_DEFINITIONS - Compiler switches required for using cppad
 
-FIND_PATH(CppAD_INCLUDE_DIR
+FIND_PATH(cppad_INCLUDE_DIR
   NAMES cppad/configure.hpp
-  PATHS ${CppAD_PREFIX}
+  PATHS ${cppad_PREFIX}
   )
-FIND_LIBRARY(CppAD_LIBRARY
+FIND_LIBRARY(cppad_LIBRARY
   NAMES cppad_lib
-  PATHS ${CppAD_PREFIX}
+  PATHS ${cppad_PREFIX}
   )
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CppAD DEFAULT_MSG CppAD_LIBRARY CppAD_INCLUDE_DIR)
-mark_as_advanced(CppAD_INCLUDE_DIR CppAD_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(cppad DEFAULT_MSG cppad_LIBRARY cppad_INCLUDE_DIR)
+mark_as_advanced(cppad_INCLUDE_DIR cppad_LIBRARY)
