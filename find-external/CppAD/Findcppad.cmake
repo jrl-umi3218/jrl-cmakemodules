156eb83e3ce95652d6642b8a3624c6a0efb10050
Rohan Budhiraja <proyan@users.noreply.github.com>
2020-08-06 11:39:20 +0200

    [find-external/cppad] add version checking

--- find-external/CppAD/Findcppad.cmake
+++ find-external/CppAD/Findcppad.cmake
@@ -1,7 +1,7 @@
 #
 #   Copyright 2020 CNRS INRIA
 #
-#   Author: Guilhem Saurel
+#   Author: Guilhem Saurel, Rohan Budhiraja
 #
 
 # Try to find cppad
@@ -11,6 +11,7 @@
 #  cppad_INCLUDE_DIR - The cppad include directories
 #  cppad_LIBRARY - The libraries needed to use cppad
 #  cppad_DEFINITIONS - Compiler switches required for using cppad
+#  cppad_VERSION - Version of cppad found
 
 FIND_PATH(cppad_INCLUDE_DIR
   NAMES cppad/configure.hpp
@@ -21,6 +22,13 @@ FIND_LIBRARY(cppad_LIBRARY
   PATHS ${cppad_PREFIX}
   )
 
+IF(cppad_INCLUDE_DIR AND EXISTS "${cppad_INCLUDE_DIR}/cppad/configure.hpp")
+  file(STRINGS "${cppad_INCLUDE_DIR}/cppad/configure.hpp" cppad_version_str
+    REGEX "^# *define[\t ]+CPPAD_PACKAGE_STRING[\t ]+\"cppad-.*\"")
+  string(REGEX REPLACE "^# *define[\t ]+CPPAD_PACKAGE_STRING[\t ]+\"cppad-([^\"]*)\".*" "\\1"
+    cppad_VERSION "${cppad_version_str}")
+ENDIF()
+
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(cppad DEFAULT_MSG cppad_LIBRARY cppad_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(cppad REQUIRED_VARS cppad_LIBRARY cppad_INCLUDE_DIR VERSION_VAR cppad_VERSION)
 mark_as_advanced(cppad_INCLUDE_DIR cppad_LIBRARY)
