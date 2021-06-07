50e960b810bde62ea1931f2471e68d391c9b2540
Wolfgang Merkt <w.merkt@gmail.com>
2021-06-07 13:35:41 +0100

    [package-config] Remove code duplication and include cxx-standard when required

--- package-config.cmake
+++ package-config.cmake
@@ -136,50 +136,16 @@ string(REPLACE ";" "\n  " PACKAGE_DEPENDENCIES_FIND_PACKAGE "${_PACKAGE_CONFIG_D
 string(REPLACE ";" "\n  " PACKAGE_DEPENDENCIES_FIND_DEPENDENCY "${_PACKAGE_CONFIG_DEPENDENCIES_FIND_DEPENDENCY}")
 
 if(DEFINED _MINIMAL_CXX_STANDARD)
-  set(PACKAGE_EXTRA_MACROS "${PACKAGE_EXTRA_MACROS}
-set(_MINIMAL_CXX_STANDARD ${_MINIMAL_CXX_STANDARD})
-set(MINIMAL_CXX_STANDARD_ENFORCE ${MINIMAL_CXX_STANDARD_ENFORCE})
+  # Read macro file and append
+  file(READ ${PROJECT_JRL_CMAKE_MODULE_DIR}/cxx-standard.cmake CXX_STANDARD_MACRO_CONTENT)
+  set(PACKAGE_EXTRA_MACROS "${PACKAGE_EXTRA_MACROS}\n# C++ standard compatibility check/enforcement\nset(_MINIMAL_CXX_STANDARD ${_MINIMAL_CXX_STANDARD})\n\n${CXX_STANDARD_MACRO_CONTENT}")
 
-# Get compiler default cxx standard, by printing \"__cplusplus\" (only once)
-if(NOT DEFINED _COMPILER_DEFAULT_CXX_STANDARD)
-  if(MSVC)
-    # See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
-    string(APPEND CMAKE_CXX_FLAGS \" /Zc:__cplusplus\")
-  endif()
-  
-  write_file($\{CMAKE_CURRENT_BINARY_DIR\}/tmp-cxx-standard.cpp \"#include <iostream>\\nint main(){std::cout << __cplusplus;return 0;}\")
-  try_run(_cxx_standard_run_status _cxx_standard_build_status
-    $\{CMAKE_CURRENT_BINARY_DIR\} $\{CMAKE_CURRENT_BINARY_DIR\}/tmp-cxx-standard.cpp
-    RUN_OUTPUT_VARIABLE _COMPILER_DEFAULT_CXX_STANDARD)
-  message(STATUS \"Default C++ standard: $\{_COMPILER_DEFAULT_CXX_STANDARD\}\")
-endif()
-
-# Check if a non-trivial minimum has been requested
-if(DEFINED _MINIMAL_CXX_STANDARD AND NOT _MINIMAL_CXX_STANDARD EQUAL 98)
-  # Check that the requested minimum is higher than the compiler default
-  # ref https://en.cppreference.com/w/cpp/preprocessor/replace#Predefined_macros for constants
-  if(_COMPILER_DEFAULT_CXX_STANDARD EQUAL 199711
-      OR (_COMPILER_DEFAULT_CXX_STANDARD EQUAL 201103 AND _MINIMAL_CXX_STANDARD GREATER 11)
-      OR (_COMPILER_DEFAULT_CXX_STANDARD EQUAL 201402 AND _MINIMAL_CXX_STANDARD GREATER 14)
-      OR (_COMPILER_DEFAULT_CXX_STANDARD EQUAL 201703 AND _MINIMAL_CXX_STANDARD GREATER 17)
-      OR (DEFINED CMAKE_CXX_STANDARD AND CMAKE_CXX_STANDARD LESS 98 AND _MINIMAL_CXX_STANDARD GREATER CMAKE_CXX_STANDARD)
-      OR (DEFINED CMAKE_CXX_STANDARD AND CMAKE_CXX_STANDARD EQUAL 98 AND _MINIMAL_CXX_STANDARD LESS CMAKE_CXX_STANDARD))
-    message(STATUS \"Incompatible C++ standard detected: Current standard is $\{CMAKE_CXX_STANDARD\} and minimal is $\{_MINIMAL_CXX_STANDARD\}\")
-    # Check that the requested minimum is higher than any pre-existing CMAKE_CXX_STANDARD
-    if(NOT CMAKE_CXX_STANDARD OR CMAKE_CXX_STANDARD EQUAL 98 OR CMAKE_CXX_STANDARD LESS _MINIMAL_CXX_STANDARD)
-      if(ENFORCE_MINIMAL_CXX_STANDARD OR MINIMAL_CXX_STANDARD_ENFORCE)
-        set(ORIGINAL_CMAKE_CXX_STANDARD ${CMAKE_CXX_STANDARD})
-        set(CMAKE_CXX_STANDARD ${_MINIMAL_CXX_STANDARD})
-        message(AUTHOR_WARNING \"CMAKE_CXX_STANDARD automatically upgraded to $\{CMAKE_CXX_STANDARD\} (from $\{ORIGINAL_CMAKE_CXX_STANDARD\})\")
-      else()
-        message(FATAL_ERROR \"CMAKE_CXX_STANDARD upgrade to >= $\{_MINIMAL_CXX_STANDARD\} required\")
-      endif()
-    endif()
+  # Add check for standard - enforce if required
+  if(${MINIMAL_CXX_STANDARD_ENFORCE})
+    set(PACKAGE_EXTRA_MACROS "${PACKAGE_EXTRA_MACROS}\nCHECK_MINIMAL_CXX_STANDARD(${_MINIMAL_CXX_STANDARD} ENFORCE)")
   else()
-    message(STATUS \"C++ standard sufficient: Minimal required $\{_MINIMAL_CXX_STANDARD\}, currently defined: $\{CMAKE_CXX_STANDARD\}\")
+    set(PACKAGE_EXTRA_MACROS "${PACKAGE_EXTRA_MACROS}\nCHECK_MINIMAL_CXX_STANDARD(${_MINIMAL_CXX_STANDARD})")
   endif()
-endif()
-")
 endif()
 
 # Configure '<PROJECT-NAME>ConfigVersion.cmake'
