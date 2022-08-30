959c164ccef9cfb385788ba851b5e7746b987360
Wolfgang Merkt <wxmerkt@users.noreply.github.com>
2022-08-30 08:32:54 +0100

    Change FindBoost warning for newer versions to STATUS

--- boost/FindBoost.cmake
+++ boost/FindBoost.cmake
@@ -844,7 +844,7 @@ function(_Boost_COMPONENT_DEPENDENCIES component _ret)
       set(_Boost_WSERIALIZATION_DEPENDENCIES serialization)
     endif()
     if(NOT Boost_VERSION VERSION_LESS 106900)
-      message(WARNING "New Boost version may have incorrect or missing dependencies and imported targets")
+      message(STATUS "New Boost version may have incorrect or missing dependencies and imported targets")
     endif()
   endif()
 
