db2698d42db1c5ca00e99b8fbd1e43382b0d260f
fabinsch <fabi.schramm@gmail.com>
2022-09-09 17:05:10 +0200

    pyproject: catch import error

--- pyproject.py
+++ pyproject.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
-
-import tomlkit
+try:
+    import tomlkit
+except ImportError as e:
+    print('tomlkit not found. Please make sure to install the package. If it is already installed, make sure that it is included in the search path.')
+    raise e
 
 
 def update_pyproject_version(version):
@@ -17,3 +20,4 @@ if __name__ == "__main__":
         update_pyproject_version(sys.argv[1])
     else:
         print("you must provide the version as argument.")
+        exit(1)
