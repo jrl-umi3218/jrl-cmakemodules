41a3e8afb7ca66860341dc8a8d7c9c977496e7e2
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-09-14 11:36:15 +0900

    Add @PACKAGE_CPPNAME@_DEPRECATED_HEADER macro

--- deprecated.hh.cmake
+++ deprecated.hh.cmake
@@ -39,4 +39,16 @@
 #  endif // __MSVC__
 # endif // __cplusplus
 
+# if defined(__GNUC__) || defined(__clang__)
+#  define @PACKAGE_CPPNAME@_PRAGMA(X) _Pragma(#X)
+#  define @PACKAGE_CPPNAME@_DEPRECATED_HEADER(MSG) @PACKAGE_CPPNAME@_PRAGMA(GCC warning MSG)
+# elif defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#  define @PACKAGE_CPPNAME@_STRINGIZE_(MSG) #MSG
+#  define @PACKAGE_CPPNAME@_STRINGIZE(MSG) @PACKAGE_CPPNAME@_STRINGIZE_(MSG)
+#  define @PACKAGE_CPPNAME@_DEPRECATED_HEADER(MSG) \
+    __pragma(message(__FILE__ "(" @PACKAGE_CPPNAME@_STRINGIZE(__LINE__) ") : Warning: " MSG))
+# else
+#  define @PACKAGE_CPPNAME@_DEPRECATED_HEADER(MSG)
+# endif
+
 #endif //! @PACKAGE_CPPNAME@_DEPRECATED_HH
