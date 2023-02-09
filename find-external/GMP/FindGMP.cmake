3780e6ea0ab85b9bf3724f0dfb3359487d5cce38
Justin Carpentier <justin.carpentier@inria.fr>
2023-02-09 19:00:02 +0100

    external/GMP: fix find strategy

--- find-external/GMP/FindGMP.cmake
+++ find-external/GMP/FindGMP.cmake
@@ -10,15 +10,19 @@ find_path(
   NAMES gmp.h
   PATHS $ENV{GMPDIR} ${INCLUDE_INSTALL_DIR})
 
-find_library(GMP_LIBRARIES gmp PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
+find_library(
+  GMP_LIBRARIES
+  NAMES gmp
+  PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
+find_package_handle_standard_args(gmp DEFAULT_MSG GMP_INCLUDES GMP_LIBRARIES)
 
 # Set gmp target
 if(GMP_FOUND)
-  add_library(gmp INTERFACE IMPORTED ${GMP_LIBRARIES})
-  set_target_properties(gmp PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                       "${GMP_INCLUDE_DIR}")
+  add_library(gmp::gmp SHARED IMPORTED)
+  set_target_properties(
+    gmp::gmp PROPERTIES IMPORTED_LOCATION ${GMP_LIBRARIES}
+                        INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}")
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDES GMP_LIBRARIES)
 mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
