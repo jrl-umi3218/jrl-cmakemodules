ee1ed325977c9b9fa47c494f8a7c749850c88d86
Justin Carpentier <justin.carpentier@inria.fr>
2023-05-31 08:47:22 +0200

    python: split helpers and finders

---
+++ python-helpers.cmake
@@ -0,0 +1,108 @@
+# Copyright (C) 2008-2023 LAAS-CNRS, JRL AIST-CNRS, INRIA.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# .rst: .. command::  PYTHON_INSTALL(MODULE FILE DEST)
+#
+# Compile and install a Python file.
+#
+macro(PYTHON_INSTALL MODULE FILE DEST)
+
+  python_build("${MODULE}" "${FILE}")
+
+  install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}/${FILE}"
+          DESTINATION "${DEST}/${MODULE}")
+endmacro()
+
+# .rst: .. command:: PYTHON_INSTALL_ON_SITE (MODULE FILE)
+#
+# Compile and install a Python file in :cmake:variable:`PYTHON_SITELIB`.
+#
+macro(PYTHON_INSTALL_ON_SITE MODULE FILE)
+  python_install("${MODULE}" "${FILE}" ${PYTHON_SITELIB})
+endmacro()
+
+# PYTHON_BUILD(MODULE FILE DEST)
+# --------------------------------------
+#
+# Build a Python file from the source directory in the build directory.
+#
+macro(PYTHON_BUILD MODULE FILE)
+message(STATUS "PYTHON_BUILD")
+  # Regex from IsValidTargetName in CMake/Source/cmGeneratorExpression.cxx
+  string(REGEX REPLACE "[^A-Za-z0-9_.+-]" "_" compile_pyc
+                       "compile_pyc_${CMAKE_CURRENT_SOURCE_DIR}")
+  if(NOT TARGET ${compile_pyc})
+    add_custom_target(${compile_pyc} ALL)
+  endif()
+
+  set(INPUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}/${FILE}")
+  message(STATUS "INPUT_FILE: ${INPUT_FILE}")
+  if(CMAKE_GENERATOR MATCHES "Visual Studio")
+    SET(OUTPUT_FILE_DIR "${CMAKE_CURRENT_BINARY_DIR}/${MODULE}/$<CONFIG>") 
+  else()
+    SET(OUTPUT_FILE_DIR "${CMAKE_CURRENT_BINARY_DIR}/${MODULE}") 
+  endif(CMAKE_GENERATOR MATCHES "Visual Studio")
+  set(OUTPUT_FILE "${OUTPUT_FILE_DIR}/${FILE}c")
+  message(STATUS "OUTPUT_FILE: ${OUTPUT_FILE}")
+
+  file(MAKE_DIRECTORY ${OUTPUT_FILE_DIR})
+
+  add_custom_command(
+    TARGET ${compile_pyc}
+    PRE_BUILD
+    COMMAND
+    ${PYTHON_EXECUTABLE} -c "import py_compile; py_compile.compile(\"${INPUT_FILE}\",\"${OUTPUT_FILE}\", doraise=True)"
+    VERBATIM)
+
+  # Tag pyc file as generated.
+  set_source_files_properties(${OUTPUT_FILE}
+    PROPERTIES GENERATED TRUE)
+
+  # Clean generated files.
+  set_property(
+    DIRECTORY
+    APPEND
+    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${OUTPUT_FILE})
+endmacro()
+
+# PYTHON_BUILD_FILE(FILE)
+# --------------------------------------
+#
+# Build a Python a given file.
+#
+macro(PYTHON_BUILD_FILE FILE)
+  # Regex from IsValidTargetName in CMake/Source/cmGeneratorExpression.cxx
+  string(REGEX REPLACE " [^A-Za-z0-9_.+-]" "_ " compile_pyc
+                       " compile_pyc_${CMAKE_CURRENT_SOURCE_DIR} ")
+  if(NOT TARGET ${compile_pyc})
+    add_custom_target(${compile_pyc} ALL)
+  endif()
+
+  add_custom_command(
+    TARGET ${compile_pyc}
+    PRE_BUILD
+    COMMAND " ${PYTHON_EXECUTABLE} " -c
+            " import py_compile; py_compile.compile(\"${FILE}\",\"${FILE}c\") "
+    VERBATIM)
+
+  # Tag pyc file as generated.
+  set_source_files_properties(" ${FILE}c " PROPERTIES GENERATED TRUE)
+
+  # Clean generated files.
+  set_property(
+    DIRECTORY
+    APPEND
+    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES " ${FILE}c ")
+endmacro()
