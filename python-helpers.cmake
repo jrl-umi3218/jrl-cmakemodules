d148904e417de9d548abec3456c914723a82a45e
Justin Carpentier <justin.carpentier@inria.fr>
2023-06-03 23:44:59 +0200

    python: add PYTHON_BUILD_GET_TARGET function
    It simplifies the code and get access to the created target

--- python-helpers.cmake
+++ python-helpers.cmake
@@ -32,19 +32,31 @@ macro(PYTHON_INSTALL_ON_SITE MODULE FILE)
   python_install("${MODULE}" "${FILE}" ${PYTHON_SITELIB})
 endmacro()
 
+# PYTHON_BUILD_GET_TARGET(TARGET)
+# -----------------------------------------
+#
+# Get the target associated to the PYTHON_BUILD procedure
+#
+function(PYTHON_BUILD_GET_TARGET python_build_target)
+  # Regex from IsValidTargetName in CMake/Source/cmGeneratorExpression.cxx
+  string(REGEX REPLACE "[^A-Za-z0-9_.+-]" "_" compile_pyc
+    "compile_pyc_${CMAKE_CURRENT_SOURCE_DIR}")
+
+  if(NOT TARGET ${compile_pyc})
+    add_custom_target(${compile_pyc} ALL)
+  endif()
+
+  set(${python_build_target} ${compile_pyc} PARENT_SCOPE)
+endfunction(PYTHON_BUILD_GET_TARGET NAME)
+
 # PYTHON_BUILD(MODULE FILE DEST)
 # --------------------------------------
 #
 # Build a Python file from the source directory in the build directory.
 #
 macro(PYTHON_BUILD MODULE FILE)
-  # Regex from IsValidTargetName in CMake/Source/cmGeneratorExpression.cxx
-  string(REGEX REPLACE "[^A-Za-z0-9_.+-]" "_" compile_pyc
-                       "compile_pyc_${CMAKE_CURRENT_SOURCE_DIR}")
-
-  if(NOT TARGET ${compile_pyc})
-    add_custom_target(${compile_pyc} ALL)
-  endif()
+  set(python_build_target "")
+  PYTHON_BUILD_GET_TARGET(python_build_target)
 
   set(INPUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}/${FILE}")
 
@@ -59,7 +71,7 @@ macro(PYTHON_BUILD MODULE FILE)
   # Create directory accounting for the generator expression contained in
   # ${OUTPUT_FILE_DIR}
   add_custom_command(
-    TARGET ${compile_pyc}
+    TARGET ${python_build_target}
     PRE_BUILD
     COMMAND ${CMAKE_COMMAND} -E make_directory "${OUTPUT_FILE_DIR}")
 
@@ -72,13 +84,8 @@ endmacro()
 # Build a Python a given file.
 #
 macro(PYTHON_BUILD_FILE FILE)
-  # Regex from IsValidTargetName in CMake/Source/cmGeneratorExpression.cxx
-  string(REGEX REPLACE "[^A-Za-z0-9_.+-]" "_" compile_pyc
-                       "compile_pyc_${CMAKE_CURRENT_SOURCE_DIR}")
-
-  if(NOT TARGET ${compile_pyc})
-    add_custom_target(${compile_pyc} ALL)
-  endif()
+  set(python_build_target "")
+  PYTHON_BUILD_GET_TARGET(python_build_target)
 
   set(extra_var "${ARGV1}")
   if(NOT extra_var STREQUAL "")
@@ -88,11 +95,11 @@ macro(PYTHON_BUILD_FILE FILE)
   endif()
 
   add_custom_command(
-    TARGET ${compile_pyc}
+    TARGET ${python_build_target}
     PRE_BUILD
     COMMAND
-      "${PYTHON_EXECUTABLE}" -c
-      "import py_compile; py_compile.compile(\"${FILE}\",\"${OUTPUT_FILE}\")"
+    "${PYTHON_EXECUTABLE}" -c
+    "import py_compile; py_compile.compile(\"${FILE}\",\"${OUTPUT_FILE}\")"
     VERBATIM)
 
   # Tag pyc file as generated.
