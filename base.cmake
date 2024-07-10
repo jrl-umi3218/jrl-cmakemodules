788666924c0b66dd84dffcdc0f3ba7c949979983
Joris Vaillant <joris.vaillant@inria.fr>
2024-07-10 10:57:04 +0200

    base: Fix version_compute if PROJECT_SOURCE_DIR is already set to the wrong project

--- base.cmake
+++ base.cmake
@@ -195,7 +195,7 @@ message(STATUS "Configuring \"${PROJECT_NAME}\" (${PROJECT_URL})")
 
 # If the project version number is not set, compute it automatically.
 if(NOT DEFINED PROJECT_VERSION)
-  version_compute()
+  version_compute(PROJECT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/..)
 else()
   if(NOT DEFINED PROJECT_VERSION_MAJOR
      AND NOT DEFINED PROJECT_VERSION_MINOR
