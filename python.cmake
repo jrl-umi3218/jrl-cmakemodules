53183f8d91aaaaa6e2fd4efaa8a5e65e58338339
ManifoldFR <wilson.jallet@polytechnique.org>
2025-05-02 14:40:12 +0200

    update copyright years

--- python.cmake
+++ python.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2008-2024 LAAS-CNRS, JRL AIST-CNRS, INRIA.
+# Copyright (C) 2008-2025 LAAS-CNRS, JRL AIST-CNRS, INRIA.
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
