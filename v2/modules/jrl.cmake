3b5c7bb95dcdf23186a5670060ce34374e0e6458
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-01-06 17:19:06 +0100

    merge check python module name into main module

--- v2/modules/jrl.cmake
+++ v2/modules/jrl.cmake
@@ -2095,13 +2095,59 @@ endfunction()
 # Usage: jrl_check_python_module_name(<module_target>)
 function(jrl_check_python_module_name target)
     jrl_check_target_exists(${target})
+    set(script ${CMAKE_BINARY_DIR}/generated/cmake/${PROJECT_NAME}/check-python-module-name.cmake)
+    file(
+        CONFIGURE
+        OUTPUT ${script}
+        @ONLY
+        CONTENT
+            [[
+if(NOT CMAKE_SCRIPT_MODE_FILE)
+    message(FATAL_ERROR "This script is intended to be run in script mode only. Use -P <script>.")
+endif()
+
+if(NOT DEFINED MODULE_FILE)
+    message(
+        FATAL_ERROR
+        "MODULE_FILE variable is not defined, please pass -DMODULE_FILE=<path_to_module_file> to the script"
+    )
+endif()
+
+if(NOT DEFINED EXPECTED_MODULE_NAME)
+    message(FATAL_ERROR "MODULE_FILE variable is not defined.")
+endif()
+
+file(STRINGS "${MODULE_FILE}" target_content REGEX "PyInit_([a-zA-Z0-9_]+)" LIMIT_COUNT 1)
+string(REGEX MATCH "PyInit_([a-zA-Z0-9_]+)" _ "${target_content}")
+
+if(NOT CMAKE_MATCH_1)
+    message(
+        FATAL_ERROR
+        "Could not find PyInit function in module file: '${MODULE_FILE}'. Is this a valid Python module?"
+    )
+endif()
+
+if(NOT CMAKE_MATCH_1 STREQUAL EXPECTED_MODULE_NAME)
+    message(
+        FATAL_ERROR
+        "Module name mismatch for module file: '${MODULE_FILE}'. "
+        "Expected: '${EXPECTED_MODULE_NAME}', Detected: '${CMAKE_MATCH_1}'"
+    )
+endif()
+
+message(
+    DEBUG
+    "Python module name check passed for '${MODULE_FILE}'. Detected module name: '${CMAKE_MATCH_1}'"
+)
+]]
+    )
 
     add_custom_command(
         TARGET ${target}
         POST_BUILD
         COMMAND
             ${CMAKE_COMMAND} -DMODULE_FILE=$<TARGET_FILE:${target}> -DEXPECTED_MODULE_NAME=${target}
-            -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CheckPythonModuleNameScript.cmake
+            -P ${script}
         COMMENT "Checking 'PyInit_${target}' exists"
         VERBATIM
     )
