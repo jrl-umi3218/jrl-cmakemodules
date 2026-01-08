837e1a5ac3774c3f47a76becd7200ca4a8f57446
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-01-08 18:09:28 +0100

    fix pytest on ubuntu 22.04 its named pytest-3 and prints version in stderr. go figure.

--- v2/find-modules/FindPytest.cmake
+++ v2/find-modules/FindPytest.cmake
@@ -10,20 +10,27 @@ if(NOT TARGET Python::Interpreter)
     )
 endif()
 
-# If python is installed via vcpkg and pytest installed via
+# Windows: If python is installed via vcpkg and pytest installed via
 # C:/vcpkg/installed/x64-windows/tools/python3/python.exe -m pip install pytest
 # Then pytest will be located in C:/vcpkg/installed/x64-windows/tools/python3/Scripts/pytest.exe
 # So we add an additional hint to find_program
-
 cmake_path(GET Python_EXECUTABLE PARENT_PATH Python_ROOT)
-find_program(Pytest_EXECUTABLE pytest HINTS ${Python_ROOT}/Scripts REQUIRED)
 
+# On Ubuntu 22.04, pytest is named pytest-3
+find_program(Pytest_EXECUTABLE NAMES pytest pytest-3 HINTS ${Python_ROOT}/Scripts REQUIRED)
+
+# On Ubuntu 22.04, pytest prints the version in stderr
 execute_process(
     COMMAND ${Pytest_EXECUTABLE} --version
     OUTPUT_VARIABLE Pytest_VERSION_FULL
+    ERROR_VARIABLE Pytest_VERSION_FULL
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-string(REGEX MATCH "[0-9]+(\\.[0-9]+)*" Pytest_VERSION "${Pytest_VERSION_FULL}")
+string(REGEX MATCH "[0-9]+\\.[0-9]+(\\.[0-9a-zA-Z]+)?" Pytest_VERSION "${Pytest_VERSION_FULL}")
+
+if(NOT Pytest_VERSION)
+    message(FATAL_ERROR "Could not determine Pytest version from output: '${Pytest_VERSION_FULL}'")
+endif()
 
 mark_as_advanced(Pytest_EXECUTABLE Pytest_VERSION)
 
