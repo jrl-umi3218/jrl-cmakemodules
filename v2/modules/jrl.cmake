5d67f2b73f8d84c48cf797d827e8a5de81b3fb7b
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-02-16 17:39:06 +0100

    cmake: update python and nanobind min required

--- v2/modules/jrl.cmake
+++ v2/modules/jrl.cmake
@@ -3064,7 +3064,7 @@ jrl_find_python(
 
 ### Example
 ```cmake
-jrl_find_python(3.8 REQUIRED COMPONENTS Interpreter Development.Module)
+jrl_find_python(3.10 REQUIRED COMPONENTS Interpreter Development.Module)
 ```
 #]============================================================================]
 macro(jrl_find_python)
@@ -3107,7 +3107,7 @@ jrl_find_nanobind([<args>...])
 
 ### Example
 ```cmake
-jrl_find_python(3.8 REQUIRED COMPONENTS Interpreter Development.Module)
+jrl_find_python(3.10 REQUIRED COMPONENTS Interpreter Development.Module)
 jrl_find_nanobind(2.5.0 CONFIG REQUIRED)
 ```
 #]============================================================================]
