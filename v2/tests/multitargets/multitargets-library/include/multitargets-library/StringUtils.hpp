955163a8c2fd7b5657e901c285627075566fef9b
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2026-03-17 15:17:56 +0100

    tests: refactor current testsuite and add multitargets test

---
+++ v2/tests/multitargets/multitargets-library/include/multitargets-library/StringUtils.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace multitargets_library {
+
+class StringUtils {
+ public:
+  std::string brackets(const std::string& str);
+  std::string reverse(const std::string& str);
+};
+
+}  // namespace multitargets_library
---
+++ v2/tests/multitargets/multitargets-library/src/Math.cpp
@@ -0,0 +1,9 @@
+#include "multitargets-library/Math.hpp"
+
+namespace multitargets_library {
+
+int Math::add(int a, int b) { return a + b; }
+
+int Math::multiply(int a, int b) { return a * b; }
+
+}  // namespace multitargets_library
