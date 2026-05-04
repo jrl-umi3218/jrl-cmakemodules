736f179396050935f0a7615e83b2c73349083fb5
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-04 12:37:50 +0200

    nix: switch to flakoboros

--- flake.nix
+++ flake.nix
@@ -1,46 +1,20 @@
 {
   description = "CMake utility toolbox";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
-      { self, lib, ... }:
+    inputs.gepetto.lib.mkFlakoboros inputs (
+      { lib, ... }:
       {
-        systems = lib.systems.flakeExposed;
-        flake.overlays = {
-          default = final: prev: {
-            jrl-cmakemodules = prev.jrl-cmakemodules.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.gitTracked ./.;
-              };
-            };
+        overrideAttrs.jrl-cmakemodules = {
+          patches = [ ];
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.gitTracked ./.;
           };
         };
-        perSystem =
-          {
-            pkgs,
-            self',
-            system,
-            ...
-          }:
-          {
-            _module.args = {
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                overlays = [ self.overlays.default ];
-              };
-            };
-            packages = {
-              default = self'.packages.jrl-cmakemodules;
-              jrl-cmakemodules = pkgs.jrl-cmakemodules;
-            };
-          };
       }
     );
 }
