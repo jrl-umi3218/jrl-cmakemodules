d02ee7e94b20b61c789d7850a1214fb0585495df
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-07-04 16:19:47 +0200

    v2(nix): add jrl-release tool

--- flake.nix
+++ flake.nix
@@ -8,6 +8,67 @@
     inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
+        pyPackages.cmake-parser =
+          {
+            buildPythonPackage,
+            setuptools,
+            setuptools-scm,
+            fetchPypi,
+            attrs,
+            ...
+          }:
+          buildPythonPackage rec {
+            pname = "cmake_parser";
+            version = "0.9.2";
+            pyproject = true;
+
+            build-system = [
+              setuptools
+              setuptools-scm
+            ];
+            dependencies = [ attrs ];
+
+            src = fetchPypi {
+              inherit pname version;
+              hash = "sha256-t6MT0/QeWMCeCIbyyY8/zuKxiX/n+HRJgjpT5RqyOj0=";
+            };
+          };
+
+        pyPackages.jrl-release =
+          {
+            buildPythonPackage,
+            cmake-parser,
+            uv-build,
+            setuptools,
+            tomlkit,
+            ruamel-yaml,
+            rich,
+            packaging,
+            ...
+          }:
+          buildPythonPackage {
+            inherit ((lib.importTOML ./v2/scripts/pyproject.toml).project) name version;
+            pyproject = true;
+
+            src = lib.cleanSource ./v2/scripts;
+
+            build-system = [
+              uv-build
+              setuptools
+            ];
+
+            dependencies = [
+              tomlkit
+              ruamel-yaml
+              rich
+              packaging
+              cmake-parser
+            ];
+            meta = {
+              mainProgram = "jrl-release";
+            };
+          };
+
         overrideAttrs.jrl-cmakemodules =
           { pkgs-final, ... }:
           {
